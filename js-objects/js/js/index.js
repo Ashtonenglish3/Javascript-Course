836194f0fe7b3a013fa1a31c4c6a6fb8a94541be
Ashtonenglish3 <ashtonenglish3@gmail.com>
2020-04-07 15:06:34 -0700

    learnt about objects constructors

--- js-objects/js/js/index.js
+++ js-objects/js/js/index.js
@@ -12,12 +12,41 @@
 // console.log(person.age);
 
 
+//object constructors is multiple objects
 
-let person = {
-    name: "Daniel",
-    eyeColor: "Blue",
-    age: 27,
-    updateAge: function(){
-        return ++person.age;
-    }
+
+// let person = {
+//     name: "Daniel",
+//     eyeColor: "Blue",
+//     age: 27,
+//     updateAge: function(){
+//         return ++person.age;
+//     }
+// }
+
+
+// let person02 = {
+//     name: "Daniel",
+//     eyeColor: "Blue",
+//     age: 27,
+//     updateAge: function(){
+//         return ++person.age;
+//     }
+// }
+
+
+// Object blueprint
+// objects always start with capital letters
+function Person(name, eyeColor, age){
+    this.name = name; //name is the placeholder
+    this.eyeColor = eyeColor;
+    this.age = age;
+    this.updateAge = function(){
+        return ++this.age;
+    };
+
 }
+
+let person01 = new Person("Daniel", "Blue", 27);
+
+console.log(person01);
