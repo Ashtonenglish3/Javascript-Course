b71b7c572cb755719c8ebab49ac51b7f52ba8caa
Ashtonenglish3 <ashtonenglish3@gmail.com>
2020-04-07 14:58:00 -0700

    learnt about objects and displaying characteristics on console

---
+++ js-objects/js/js/index.js
@@ -0,0 +1,23 @@
+// let person = new Object();
+
+// person.name = 'Daniel';
+// person.eyeColor = 'Blue';
+// person.age = 27;
+// person.updateAge = function(){
+//     return ++person.age;
+// }
+
+// console.log(person.age);
+// person.updateAge();
+// console.log(person.age);
+
+
+
+let person = {
+    name: "Daniel",
+    eyeColor: "Blue",
+    age: 27,
+    updateAge: function(){
+        return ++person.age;
+    }
+}
